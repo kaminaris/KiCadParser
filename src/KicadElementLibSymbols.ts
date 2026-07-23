c80e7d9a946825e0007eed2b43eab597c4d303e0
Pawel <zacdbg@gmail.com>
2026-07-23 20:11:44 +0200

    add datasheet symbol extraction feature with API integration

--- src/KicadElementLibSymbols.ts
+++ src/KicadElementLibSymbols.ts
@@ -2,6 +2,8 @@ import { KicadElementSymbol } from './KicadElementSymbol';
 import { KicadElement }       from './KicadElement';
 
 export class KicadElementLibSymbols extends KicadElement {
+	override name = 'lib_symbols';
+
 	findSymbolByName(name: string) {
 		return this.findChildrenByClass(KicadElementSymbol).find(s => s.symbolName === name);
 	}
